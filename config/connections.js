a9a0e96b963d2b388e19aa237e50fc2e2491dcb8
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-06-29 11:12:10 -0400

    making the mysql connection and adding to gitignore

--- config/connections.js
+++ config/connections.js
@@ -5,3 +5,45 @@
  * 
  * 06-29-19
  ***********************/
+
+ var mysql = require('mysql');
+ var fs = require('fs');
+
+ // read the file for the password
+ fs.readFile('password.txt', function read(err, data) {
+     if (err) {
+         throw err;
+     }
+     password = data.toString();
+     createInfoToConnect(password)
+ });
+ 
+ // create the connection information for the sql database
+ function createInfoToConnect(password) {
+     connection = mysql.createConnection({
+         host: "localhost",
+ 
+         port: 3306,
+ 
+         user: "root",
+ 
+         password: password,
+         database: "burgers_db"
+     });
+     connect(connection);
+ }
+ 
+ 
+ // connect to the mysql server and sql database
+ function connect(connection) {
+    connection.connect(function(err) {
+        if (err) {
+          console.error("error connecting: " + err.stack);
+          return;
+        }
+        console.log("connected as id " + connection.threadId);
+      });
+ }
+
+ module.exports = connection;
+
