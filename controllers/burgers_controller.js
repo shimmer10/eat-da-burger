432bd4c162b28015aae91e012c980c9722a6a40c
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-06-29 12:31:07 -0400

    initial code for all files to return all burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,11 +6,20 @@
  * 06-29-19
  ***********************/
 
- var express = require("express");
+var express = require("express");
 
 var router = express.Router();
 
 var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        res.render("index", hbsObject);
+    });
+});
 
 
 
--- server.js
+++ server.js
@@ -8,3 +8,24 @@
 
 var express = require('express');
 
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+var handlebars = require('express-handlebars');
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
